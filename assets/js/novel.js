da93c1d564d52b9d791c361762589cc273f2591d
longphamnam99 <longkul1478@gmail.com>
2026-05-14 15:04:00 +0700

    update

--- assets/js/novel.js
+++ assets/js/novel.js
@@ -1,67 +1,32 @@
-const params = new URLSearchParams(window.location.search);
-
-const id = params.get('id');
+console.log("APP RUNNING");
 
 fetch('data/novels.json')
-.then(res => res.json())
+.then(response => response.json())
+
 .then(data => {
-
-const novel = data.find(item => item.id === id);
-
+console.log(data);
 const container =
-document.getElementById('novel-detail');
+  document.getElementById('novel-list');
+data.forEach(novel => {
+  container.innerHTML += 
+    <div class="card">
+      <img  src="${novel.cover}" alt="${novel.title}"
+      >
 
-if(!novel){
+      <h3>${novel.title}</h3>
 
-```
-container.innerHTML = `
-  <h1>Không tìm thấy truyện</h1>
-`;
+      <p>${novel.author}</p>
 
-return;
-```
+      <a
+        class="read-btn"
+        href="novel.html?id=${novel.id}"
+      >
+        Đọc Ngay
+      </a>
+    </div>  ;
 
-}
-
-container.innerHTML = `
-
-```
-<div class="detail-box">
-
-  <div class="cover">
-
-    <img
-      src="${novel.cover}"
-      alt="${novel.title}"
-    >
-
-  </div>
-
-  <div class="info">
-
-    <h1>${novel.title}</h1>
-
-    <p>
-      <b>Tác giả:</b>
-      ${novel.author}
-    </p>
-
-    <p>
-      ${novel.description}
-    </p>
-
-    <a class="read-btn" href="#">
-      Đọc Ngay
-    </a>
-
-  </div>
-
-</div>
-```
-
-`;
-
+});
 })
-.catch(error => {
+.catch(error => {console.log("FETCH ERROR:");
 console.log(error);
 });
