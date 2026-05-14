a27f7796934982cbddf231fd094bf1d556ded139
longphamnam99 <longkul1478@gmail.com>
2026-05-14 14:52:05 +0700

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,43 @@
 fetch('data/novels.json')
 .then(res => res.json())
 .then(data => {
-  const container = document.getElementById('novel-list');
 
-  data.forEach(novel => {
-    container.innerHTML += `
-      <div class="card">
-        <img src="${novel.cover}" alt="">
-        <h3>${novel.title}</h3>
-        <p>${novel.author}</p>
-        <a class="read-btn" href="novel.html?id=${novel.id}">Đọc Ngay</a>
-      </div>
-    `;
-  });
+const container = document.getElementById('novel-list');
+
+data.forEach(novel => {
+
+```
+container.innerHTML += `
+
+  <div class="card">
+
+    <div class="card-img">
+      <img src="${novel.cover}" alt="${novel.title}">
+    </div>
+
+    <div class="card-content">
+
+      <h3>${novel.title}</h3>
+
+      <p class="author">
+        ${novel.author}
+      </p>
+
+      <a class="read-btn"
+         href="novel.html?id=${novel.id}">
+         Đọc Ngay
+      </a>
+
+    </div>
+
+  </div>
+
+`;
+```
+
+});
+
+})
+.catch(error => {
+console.log(error);
 });
--- assets/js/novel.js
+++ assets/js/novel.js
@@ -1,19 +1,67 @@
 const params = new URLSearchParams(window.location.search);
+
 const id = params.get('id');
 
 fetch('data/novels.json')
 .then(res => res.json())
 .then(data => {
 
-  const novel = data.find(item => item.id === id);
+const novel = data.find(item => item.id === id);
 
-  const container = document.getElementById('novel-detail');
+const container =
+document.getElementById('novel-detail');
 
-  container.innerHTML = `
-    <div style="padding:20px">
-      <h1>${novel.title}</h1>
-      <img src="${novel.cover}" width="250">
-      <p>${novel.description}</p>
-    </div>
-  `;
+if(!novel){
+
+```
+container.innerHTML = `
+  <h1>Không tìm thấy truyện</h1>
+`;
+
+return;
+```
+
+}
+
+container.innerHTML = `
+
+```
+<div class="detail-box">
+
+  <div class="cover">
+
+    <img
+      src="${novel.cover}"
+      alt="${novel.title}"
+    >
+
+  </div>
+
+  <div class="info">
+
+    <h1>${novel.title}</h1>
+
+    <p>
+      <b>Tác giả:</b>
+      ${novel.author}
+    </p>
+
+    <p>
+      ${novel.description}
+    </p>
+
+    <a class="read-btn" href="#">
+      Đọc Ngay
+    </a>
+
+  </div>
+
+</div>
+```
+
+`;
+
+})
+.catch(error => {
+console.log(error);
 });
